e3247de6446eb1fb6fae2c14578cc6ce24d6926b
James Hassett <jhassett189@gmail.com>
2022-04-18 16:09:16 -0400

    commit tree 5a1a29cd32d4780decb0241f3e9947117f259a54

---
+++ index.js
@@ -0,0 +1,26 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function increment(n) {
+    return (++n);
+}
+function decrement(n) {
+    return (--n)
+}
+function makeInt(n) {
+    let x = parseInt(n, 10);
+    return (x)
+}
+function preserveDecimal(n) {
+    let x = parseFloat(n, 10)
+    return (x)
+}
